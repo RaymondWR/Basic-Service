80f537de1d67e0b839c7379666c041babf34cb49
Run Wang <raymond1993wr@gmail.com>
2016-02-09 16:19:51 +0000

    First Version

---
+++ app/src/main/java/com/labs/cs6313/basicservice/HelloService.java
@@ -0,0 +1,36 @@
+package com.labs.cs6313.basicservice;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.Toast;
+
+
+/**
+ * Created by wangrun on 09/02/2016.
+ */
+public class HelloService extends Service {
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started haha~", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed LOL~", Toast.LENGTH_LONG).show();
+    }
+}
